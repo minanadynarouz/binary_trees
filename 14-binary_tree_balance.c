1df24ef9d922046e960f3298ffe6dcf215360411
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-31 08:54:44 -0800

    amend

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,22 +8,22 @@
 
 size_t binary_tree_height_Balance(const binary_tree_t *tree)
 {
-        size_t heightLeft, heightRight;
+	size_t heightLeft, heightRight;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        heightLeft = 0;
-        heightRight = 0;
-        heightLeft = tree->left ? 1 + binary_tree_height_Balance(tree->left) : 1;
-        heightRight = tree->right ? 1 + binary_tree_height_Balance(tree->right) : 1;
-        return (heightLeft > heightRight ? heightLeft : heightRight);
+	heightLeft = 0;
+	heightRight = 0;
+	heightLeft = tree->left ? 1 + binary_tree_height_Balance(tree->left) : 1;
+	heightRight = tree->right ? 1 + binary_tree_height_Balance(tree->right) : 1;
+	return (heightLeft > heightRight ? heightLeft : heightRight);
 }
 
 /**
- * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * binary_tree_balance - get diff between levels
  * @tree: pointer to the root node of the tree to traverse
- * Return: size, or 0 if NULL
+ * Return: difference between levels
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
@@ -31,5 +31,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height_Balance(tree->left) - binary_tree_height_Balance(tree->right));
+	return (binary_tree_height_Balance(tree->left) -
+			binary_tree_height_Balance(tree->right));
 }
