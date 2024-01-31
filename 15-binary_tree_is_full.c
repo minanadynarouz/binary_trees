4da25d7be948a8b82ab188428fcfe548be7cf690
minanadynarouz <mina.n.narouz@gmail.com>
2024-01-30 21:59:41 -0800

    amend

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,8 +11,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
+	if ((tree->left == NULL && tree->right != NULL) ||
+			(tree->left != NULL && tree->right == NULL))
 		return (0);
 
-	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right) ? 1 : 0);
+	return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right) ? 1 : 0);
 }
